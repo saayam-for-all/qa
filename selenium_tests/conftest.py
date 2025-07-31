83a32193b9c79990d3e43e4907808f1dfa9e44bd
moddebaixa <moddebaixa@gmail.com>
2025-07-31 11:38:49 +0200

    first rearrangement

--- selenium_tests/conftest.py
+++ selenium_tests/conftest.py
@@ -6,9 +6,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope="session")
 def driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")   #new
+       
     options.add_argument("--start-maximized")
-    options.add_argument("--user-data-dir=/tmp/chrome") #new
+
+    options.add_argument("--headless") #new from here
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--user-data-dir=/tmp/chrome")  
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
