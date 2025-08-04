62ab7b15cc12010ec9a2dca2636a97ed2405a5e9
moddebaixa <moddebaixa@gmail.com>
2025-08-04 17:09:45 +0200

    multibrowser added

--- selenium_tests/conftest.py
+++ selenium_tests/conftest.py
@@ -1,21 +1,33 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-@pytest.fixture(scope="session")
-def driver():
-    options = webdriver.ChromeOptions()
-       
-    options.add_argument("--start-maximized")
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action="store", default="chrome")
 
-    options.add_argument("--headless") #new from here
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--user-data-dir=/tmp/chrome")  
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+@pytest.fixture(scope="class")
+def driver(request):
+    browser_name = request.config.getoption("browser_name")
+
+    if browser_name == "chrome":
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        print("Chrome Browser is opened")
+    elif browser_name == "firefox":
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+        print("Firefox Browser is opened")
+    elif browser_name == "edge":
+        options = webdriver.EdgeOptions()
+        driver = webdriver.Edge(EdgeChromiumDriverManager().install(), options=options)
+        print("Edge Browser is opened")
+    else:
+        raise Exception("No valid browser selected")
+
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+    driver.maximize_window()
+    driver.implicitly_wait(5)
     yield driver
     driver.quit()
