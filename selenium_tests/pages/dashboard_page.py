d5dca2f084ce45b69891206cf5b4c7f46c873f5b
Sai-Divya-Sree-NV <divyavenu2514@gmail.com>
2025-10-14 14:18:24 -0500

    Changes to 3 testcases

--- selenium_tests/pages/dashboard_page.py
+++ selenium_tests/pages/dashboard_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from login_page import LoginPage
+from pages.login_page import LoginPage
 
 
 class DashboardPage:
@@ -24,5 +24,27 @@ class DashboardPage:
     def is_loaded_cleanly(self):
         """Check dashboard loaded with no error banners or half-loaded sections"""
         banners = self.driver.find_elements(By.CSS_SELECTOR, ".error-banner")
-        main_section = self.driver.find_element(By.CSS_SELECTOR, "#main-dashboard")
+        main_section = self.driver.find_element(By.CSS_SELECTOR, "#root > div > div")
         return len(banners) == 0 and main_section.is_displayed()
+    
+    def click_create_help(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'Create Help')]"))
+        )
+        self.driver.find_element(By.XPATH,"//span[contains(text(),'Create Help')]").click()
+    
+    def verify_table_data_loaded(self):
+        rows = self.driver.find_elements(By.XPATH,"//table//tr")
+        return len(rows) > 1
+    
+    def switch_to_myrequests(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'My Requests')]"))
+        )
+        self.driver.find_element(By.XPATH,"//button[contains(text(),'My Requests')]").click()
+    
+    def switch_to_otherrequest_tab(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Others Requests')]"))
+        )
+        self.driver.find_element(By.XPATH,"//button[contains(text(),'Others Requests')]").click()
--- selenium_tests/tests/test_dashboard_page.py
+++ selenium_tests/tests/test_dashboard_page.py
@@ -1,5 +1,6 @@
 import pytest
 from logger import get_logger
+from logger import get_logger
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,7 +14,26 @@ logger = get_logger("TestDashboardPage")
 def test_dashboard_load(driver):
     logger.info("[TC_LOGIN_001] Verify dashboard page opens cleanly after login")
     page = DashboardPage(driver)
-    page.login_and_open("","")
+    page.login_and_open("saayamqa@yahoo.com","Saayamforall@123")
+    time.sleep(5)
     assert "dashboard" in driver.current_url.lower(), "Dashboard link did not navigate correctly."
+    assert page.is_loaded_cleanly(), "Dashboard did not load cleanly — found error banners or missing sections."
 
+def test_create_help_request_button(driver):
+    logger.info("[TC_DASH_002] Verify 'Create help request' button navigates correctly")
+    page = DashboardPage(driver)
+    page.login_and_open("saayamqa@yahoo.com", "Saayamforall@123")
+    page.click_create_help()
+    time.sleep(2)
+    assert "request" in driver.current_url.lower(), "Create Help Request navigation failed."
 
+def test_tabs_switching(driver):
+    logger.info("[TC_DASH_004] Validate switching between 'My Requests' and 'Other Requests'")
+    page = DashboardPage(driver)
+    page.login_and_open("saayamqa@yahoo.com", "Saayamforall@123")
+    time.sleep(5)
+    page.switch_to_otherrequest_tab()
+    assert page.verify_table_data_loaded(), "Table did not refresh after tab switch."
+    time.sleep(5)
+    page.switch_to_myrequests()
+    assert page.verify_table_data_loaded(), "Table did not refresh after tab switch."
